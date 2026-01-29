ca9a2009d86ed7f2b3bcbabb32904d53e7d07fb6
gcho <guk.cho@cern.ch>
2026-01-29 23:56:14 +0100

    Remove all files from remote repository

--- GUI/Setting/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Setting Package
-Contains settings dialog and manual dialog
-"""
-
-from .settings_dialog import SettingsDialog
-from .manual_dialog import ManualDialog
-
-__all__ = ['SettingsDialog', 'ManualDialog']
